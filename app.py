3c7986db6590a11d03fa19f4ae9193896c66f993
Mayank Singh Shishodia <23f1002944@ds.study.iitm.ac.in>
2025-09-09 23:50:13 +0530

    flask wtf

---
+++ app.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+@app.route('/index')
+@app.route('/mayank')
+def index():
+    user = {'username': 'Miguel'}
+    posts = [
+        {
+            'author': {'username': 'John'},
+            'body': 'Beautiful day in Portland!'
+        },
+        {
+            'author': {'username': 'Susan'},
+            'body': 'The Avengers movie was so cool!'
+        }
+    ]
+    return render_template('index.html', title='Home', user=user, posts=posts)
+    
+
+if __name__ == "__main__":
+    app.run(port=8000, debug=True)
+
+    
