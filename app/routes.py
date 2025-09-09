7b91dd565b9c8b0788a7bc23d6de54a751a3577e
Mayank Singh Shishodia <23f1002944@ds.study.iitm.ac.in>
2025-09-10 00:43:22 +0530

    flask forms

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,6 @@
-from flask import Flask, render_template
-
-app = Flask(__name__)
+from flask import render_template, flash, redirect
+from app import app
+from app.forms import LoginForm
 
 @app.route('/')
 @app.route('/index')
@@ -20,7 +20,10 @@ def index():
     return render_template('index.html', title='Home', user=user, posts=posts)
     
 
-if __name__ == "__main__":
-    app.run(port=8000, debug=True)
-
-    
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for user {}, remember_me={}'.format(form.username.data, form.remember_me.data))
+        return redirect('/index')
+    return render_template('login.html', title='Sign In', form=form)
