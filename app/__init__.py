c5fe4be6c587f560b99e04701b5ce7b44cfdd9be
Mayank Singh Shishodia <23f1002944@ds.study.iitm.ac.in>
2025-09-12 05:18:00 +0530

    added comments

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,16 @@
 from flask import Flask
 from .config import Config
 
-
 app = Flask(__name__)
-app.config.from_object(Config)
-
+app.config.from_object(Config) #this loads all env specific config into flask config system
+#Flask only loads uppercase attributes from configuration objects or modules because, by convention, uppercase names are used for configuration variables and constants in Python and Flask
+#(like SECRET_KEY, DEBUG, SQLALCHEMY_DATABASE_URI) clearly signal that these are intended as configuration settings or constants, not regular variables or helper functions
 
 from app import routes
+#app is a package folder
+#init is a script which runs everything when package is loaded
+#routes, config, forms are modules
+#above we import routes module into init 
 
 print("Package app is loaded")
+#Forms are not needed during initial boilerplate loading (like in __init__.py), but they will absolutely be imported in other files, such as run.py or route/view modules, whenever a web form is displayed, validated, or processed
