845d43068a0f326d0fb11ff372903036a945cf7b
Mayank Singh Shishodia <23f1002944@ds.study.iitm.ac.in>
2025-09-15 00:59:03 +0530

    databases added 2

--- app/models.py
+++ app/models.py
@@ -1,20 +1,32 @@
-from typing import Optional #this is like Generics in Java, this is type hint, makes code cleaner, Mapped[int] = type hint
-import sqlalchemy as sa #importing Core
-import sqlalchemy.orm as so #importing ORM, we can combine ORM and Core together
-from app import db #importing the object of Sqlalchemy defined in init
-
-#Optional and typing are python features, Mapped is or sqlalchemy feature
-#typing = module which helps with type hints and specifying type sof variables, args, functions and return types
-#Optional = It is a type hint from typing module, can be either a specified type or None, Optional[int] = int or None
-
+from datetime import datetime, timezone
+from typing import Optional
+import sqlalchemy as sa
+import sqlalchemy.orm as so
+from app import db
 
 class User(db.Model):
     id: so.Mapped[int] = so.mapped_column(primary_key=True)
-    username: so.Mapped[str] = so.mapped_column(sa.String(64), index=True, unique=True)
-    email: so.Mapped[str] = so.mapped_column(sa.String(120), index=True, unique=True)
+    username: so.Mapped[str] = so.mapped_column(sa.String(64), index=True,
+                                                unique=True)
+    email: so.Mapped[str] = so.mapped_column(sa.String(120), index=True,
+                                             unique=True)
     password_hash: so.Mapped[Optional[str]] = so.mapped_column(sa.String(256))
-    
+
+    posts: so.WriteOnlyMapped['Post'] = so.relationship(
+        back_populates='author')
+
     def __repr__(self):
         return '<User {}>'.format(self.username)
-    
-    
+
+class Post(db.Model):
+    id: so.Mapped[int] = so.mapped_column(primary_key=True)
+    body: so.Mapped[str] = so.mapped_column(sa.String(140))
+    timestamp: so.Mapped[datetime] = so.mapped_column(
+        index=True, default=lambda: datetime.now(timezone.utc))
+    user_id: so.Mapped[int] = so.mapped_column(sa.ForeignKey(User.id),
+                                               index=True)
+
+    author: so.Mapped[User] = so.relationship(back_populates='posts')
+
+    def __repr__(self):
+        return '<Post {}>'.format(self.body)
