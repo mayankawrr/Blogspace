2de9c61b968888c91d57dd2cff1692e490d17c7f
Mayank Singh Shishodia <23f1002944@ds.study.iitm.ac.in>
2025-09-17 23:14:15 +0530

    changed from generic style typing to pure orm based models.py

--- blogspace/models.py
+++ blogspace/models.py
@@ -1,32 +1,24 @@
 from datetime import datetime, timezone
-from typing import Optional
-import sqlalchemy as sa
-import sqlalchemy.orm as so
 from blogspace import db
 
 class User(db.Model):
-    id: so.Mapped[int] = so.mapped_column(primary_key=True)
-    username: so.Mapped[str] = so.mapped_column(sa.String(64), index=True,
-                                                unique=True)
-    email: so.Mapped[str] = so.mapped_column(sa.String(120), index=True,
-                                             unique=True)
-    password_hash: so.Mapped[Optional[str]] = so.mapped_column(sa.String(256))
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), index=True, unique=True)
+    email = db.Column(db.String(120), index=True, unique=True)
+    password_hash = db.Column(db.String(256))
 
-    posts: so.WriteOnlyMapped['Post'] = so.relationship(
-        back_populates='author')
+    posts = db.relationship('Post', back_populates='author')
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
 
 class Post(db.Model):
-    id: so.Mapped[int] = so.mapped_column(primary_key=True)
-    body: so.Mapped[str] = so.mapped_column(sa.String(140))
-    timestamp: so.Mapped[datetime] = so.mapped_column(
-        index=True, default=lambda: datetime.now(timezone.utc))
-    user_id: so.Mapped[int] = so.mapped_column(sa.ForeignKey(User.id),
-                                               index=True)
+    id = db.Column(db.Integer, primary_key=True)
+    body = db.Column(db.String(140))
+    timestamp = db.Column(db.DateTime, index=True, default=lambda: datetime.now(timezone.utc))
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), index=True)
 
-    author: so.Mapped[User] = so.relationship(back_populates='posts')
+    author = db.relationship('User', back_populates='posts')
 
     def __repr__(self):
         return '<Post {}>'.format(self.body)
